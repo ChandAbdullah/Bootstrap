3ca671197fa36f8d8fc72aa8b720f9b042236213
ChandAbdullah <chandabdullah021@gmail.com>
2020-07-12 16:31:39 +0500

    new style from client

---
+++ js/login.js
@@ -0,0 +1,23 @@
+// var x = document.getElementById("login");
+// var y = document.getElementById("signup")
+
+// function signup() {
+//     if (x.style.display === "none") {
+//         x.style.display = "none"
+//         y.style.display = "block"
+
+//     } else {
+//         x.style.display = "none"
+//         y.style.display = "block"
+//     }
+// }
+
+// function login() {
+//     if (x.style.display === 'block') {
+//         x.style.display = 'block'
+//         y.style.display = 'none'
+//     } else {
+//         x.style.display = 'block'
+//         y.style.display = 'none'
+//     }
+// }
